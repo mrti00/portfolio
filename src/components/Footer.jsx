0ec88a743c2df268c76d9cc6fde37fd2ff106565
mrti00 <micicmaarko@gmail.com>
2024-09-10 21:11:28 +0200

    small fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,6 @@ function Footer() {
       <div className="mx-auto text-center">
         <h3 className="mb-1 text-lg">Copyright 2024 &copy; Marko Micic</h3>
         <p className="text-sm">All rights reserved.</p>
-        <img
-          src="http://canarytokens.com/static/us86wg1srv9qskjdx20ddsfvc/index.html"
-          alt="Canary token"
-          style={{ display: "none" }}
-        />
       </div>
     </footer>
   );
