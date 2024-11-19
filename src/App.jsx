83df64976ce3a16f288fcac9274e939c1c6ae863
mrti00 <micicmaarko@gmail.com>
2024-11-19 01:22:47 +0100

    small fixes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Header from "./components/Header";
 
 function Preloader() {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-100">
       <div className="h-16 w-16 animate-spin rounded-full border-4 border-blue-500 border-t-transparent"></div>
     </div>
   );
